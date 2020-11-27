660a21ada340a871f6f2897eca9073116d564050
Jools Hattey <jools.hg@gmail.com>
2020-11-27 19:58:09 +0000

    restructured

--- pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Map } from "../components/Map/Map";
-
-export default function Home() {
-  return <Map></Map>;
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { useRef } from "react";
+import { Map } from "../components/Map/Map";
+import Panel from "../components/Panel/Panel";
+import { motion } from "framer-motion";
+import styles from "../components/Panel/panel.module.css";
+
+export default function Home() {
+  const constraintsRef = useRef(null);
+  return (
+    <>
+      <motion.div ref={constraintsRef} className={styles.panelContainer}>
+        <Map></Map>
+        <Panel constraints={constraintsRef}></Panel>
+      </motion.div>
+    </>
+  );
+}
