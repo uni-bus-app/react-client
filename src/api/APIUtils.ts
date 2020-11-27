89eb10661007ec4b2dbbdcd0441ae58660b9c039
Jools Hattey <jools.hg@gmail.com>
2020-11-27 22:25:46 +0000

    get times function

--- src/api/APIUtils.ts
+++ src/api/APIUtils.ts
@@ -1,19 +1,16 @@
+const apiURL = 'https://20200817t190317-dot-unibus-app.nw.r.appspot.com';
+
 export const getRoutePath = async () => {
-  const res = await fetch(
-    'https://20200817t190317-dot-unibus-app.nw.r.appspot.com/u1routepath',
-    {
-      method: 'GET',
-    }
-  );
+  const res = await fetch(`${apiURL}/u1routepath`);
   return await res.json();
 };
 
 export const getStops = async () => {
-  const res = await fetch(
-    'https://20200817t190317-dot-unibus-app.nw.r.appspot.com/stops',
-    {
-      method: 'GET',
-    }
-  );
+  const res = await fetch(`${apiURL}/stops`);
+  return await res.json();
+};
+
+export const getTimes = async (stopID: string) => {
+  const res = await fetch(`${apiURL}/times/${stopID}`);
   return await res.json();
 };
