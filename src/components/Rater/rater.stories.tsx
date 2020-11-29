7ad2f16c6bcf672e29414f56af2dba2fe26fb7a0
Reece Birchmore <reecebirchmore@gmail.com>
2020-11-29 03:10:41 +0000

    Removed unnecessary icon prop, removed controls

--- src/components/Rater/rater.stories.tsx
+++ src/components/Rater/rater.stories.tsx
@@ -1,6 +1,5 @@
 import Rater from './rater';
 import React, { useRef } from 'react';
-import { ExpandLess, ExpandMore } from '@material-ui/icons';
 
 export default {
   title: 'Rater',
@@ -12,6 +11,5 @@ const Template = (args) => <Rater {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
-  icon: [<ExpandLess />, <ExpandMore />],
   text: 'How are you finding the bus application so far?',
 };
