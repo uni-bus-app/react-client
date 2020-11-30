57f039d976d2b23e2f871fefc733435f8420bb0a
Reece Birchmore <reecebirchmore@gmail.com>
2020-11-30 17:36:32 +0000

    Rating Component: Added draggable component

--- src/components/Rater/rater.tsx
+++ src/components/Rater/rater.tsx
@@ -1,5 +1,6 @@
 import styles from './rater.module.css';
 import { Card, IconButton } from '@material-ui/core';
+import Draggable from '../Draggable/draggable';
 import {
   SentimentSatisfiedAlt,
   SentimentVeryDissatisfied,
@@ -16,21 +17,23 @@ interface RaterProps {
 export default function Rater(props: RaterProps) {
   const { text, action1, action2, action3 } = props;
   return (
-    <Card>
-      <div className={styles.container}>
-        <div className={styles.text}>{text}</div>
-        <div className={styles.ratings}>
-          <IconButton onClick={action1}>
-            <SentimentSatisfiedAlt />
-          </IconButton>
-          <IconButton onClick={action2}>
-            <SentimentSatisfied />
-          </IconButton>
-          <IconButton onClick={action3}>
-            <SentimentVeryDissatisfied />
-          </IconButton>
+    <Draggable>
+      <Card>
+        <div className={styles.container}>
+          <div className={styles.text}>{text}</div>
+          <div className={styles.ratings}>
+            <IconButton onClick={action1}>
+              <SentimentSatisfiedAlt />
+            </IconButton>
+            <IconButton onClick={action2}>
+              <SentimentSatisfied />
+            </IconButton>
+            <IconButton onClick={action3}>
+              <SentimentVeryDissatisfied />
+            </IconButton>
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </Draggable>
   );
 }
