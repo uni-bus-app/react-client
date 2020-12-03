2b0dae58f1bc516d57f46e6d28d7072e1d25b4d6
Jools Hattey <jools.hg@gmail.com>
2020-12-03 18:53:30 +0000

    move google logo with panel

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -6,12 +6,18 @@ import {
 } from '@react-google-maps/api';
 import { mapStylesLight } from './mapstyles-light';
 import { mapStylesDark } from './mapstyles-dark';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { getRoutePath, getStops } from '../../api/APIUtils';
 import { Stop } from '../../models/stop';
 import purpleStopMarker from '../../assets/stop-marker-icon-purple.svg';
 import blueStopMarker from '../../assets/stop-marker-icon-blue.svg';
 import { CSSProperties } from '@material-ui/core/styles/withStyles';
+import {
+  motion,
+  MotionValue,
+  useMotionValue,
+  useTransform,
+} from 'framer-motion';
 
 const mapOptions: google.maps.MapOptions = {
   disableDefaultUI: true,
@@ -39,6 +45,7 @@ interface MapProps {
   routeOverlayEnabled?: boolean;
   stopMarkersEnabled?: boolean;
   onMarkerSelect?: (stop: Stop) => void;
+  logoPosition: MotionValue<number>;
 }
 
 export const Map = (props: MapProps) => {
@@ -50,12 +57,20 @@ export const Map = (props: MapProps) => {
     routeOverlayEnabled,
     stopMarkersEnabled,
     onMarkerSelect,
+    logoPosition,
   } = props;
 
   const [map, setMap] = useState<google.maps.Map>();
   const [routeOverlay, setRouteOverlay] = useState<google.maps.LatLng[]>(null);
   const [stops, setStops] = useState<Stop[]>(null);
   const [selectedStop, setSelectedStop] = useState<number>();
+  const logoContainer = useRef<HTMLDivElement>();
+
+  const initialLogoPosition = useMotionValue(0);
+  const logoPos = useTransform(
+    logoPosition || initialLogoPosition,
+    (value) => value - ((process.browser && window.innerWidth) || 375)
+  );
 
   const getBounds = (
     value: google.maps.LatLngLiteral,
@@ -77,6 +92,7 @@ export const Map = (props: MapProps) => {
     setMap(map);
     const bounds = getBounds(position, 0.5);
     map.fitBounds(bounds, padding);
+    moveLogo(map);
   }, []);
   const onUnmount = useCallback(() => {
     setMap(null);
@@ -96,63 +112,97 @@ export const Map = (props: MapProps) => {
     }
   }, [position]);
 
+  const moveLogo = (map) => {
+    let isCalled = false;
+    const mapObserver = new MutationObserver((mutationList, observer) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === 'childList') {
+          const logoElement = map.__gm.Ma.querySelector('[rel="noopener"]');
+          if (logoElement) {
+            if (!isCalled) {
+              isCalled = true;
+              observer.disconnect();
+              const logoEl = logoElement.parentElement.removeChild(logoElement);
+              if (logoEl && logoContainer) {
+                logoContainer.current.append(logoEl);
+              }
+            }
+          }
+        }
+      }
+    });
+    mapObserver.observe(map.__gm.Ma, { childList: true, subtree: true });
+  };
+
   return (
-    <LoadScript googleMapsApiKey="AIzaSyDkT81ky0Yn3JYuk6bFCsq4PVmjXawppFI">
-      <GoogleMap
-        mapContainerStyle={{
-          ...(style || { width: '100vw', height: '100vh' }),
-          zIndex: 10,
+    <>
+      <motion.div
+        style={{
+          position: 'absolute',
+          zIndex: 99999,
+          bottom: '52vh',
+          left: '4vw',
+          x: logoPos,
         }}
-        options={{
-          ...mapOptions,
-          styles: darkModeEnabled ? mapStylesDark : mapStylesLight,
-        }}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        {routeOverlayEnabled && (
-          <Polyline
-            path={routeOverlay}
-            options={{
-              strokeColor: darkModeEnabled ? '#03A9F4' : '#7B1FA2',
-              strokeOpacity: 0.75,
-            }}
-          />
-        )}
-        {stopMarkersEnabled &&
-          stops?.map((stop, index) => {
-            return (
-              <Marker
-                key={index}
-                position={stop.location}
-                options={{
-                  icon: {
-                    url:
-                      (darkModeEnabled && selectedStop !== index) ||
-                      (!darkModeEnabled && selectedStop === index)
-                        ? blueStopMarker
-                        : purpleStopMarker,
-                    scaledSize: new google.maps.Size(35, 50),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(17.5, 50),
-                  },
-                }}
-                onClick={() => {
-                  setSelectedStop(index);
-                  const bounds = getBounds(
-                    {
-                      lat: stop.location.lat(),
-                      lng: stop.location.lng(),
+        ref={logoContainer}
+      ></motion.div>
+      <LoadScript googleMapsApiKey="AIzaSyDkT81ky0Yn3JYuk6bFCsq4PVmjXawppFI">
+        <GoogleMap
+          mapContainerStyle={{
+            ...(style || { width: '100vw', height: '100vh' }),
+            zIndex: 10,
+          }}
+          options={{
+            ...mapOptions,
+            styles: darkModeEnabled ? mapStylesDark : mapStylesLight,
+          }}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+        >
+          {routeOverlayEnabled && (
+            <Polyline
+              path={routeOverlay}
+              options={{
+                strokeColor: darkModeEnabled ? '#03A9F4' : '#7B1FA2',
+                strokeOpacity: 0.75,
+              }}
+            />
+          )}
+          {stopMarkersEnabled &&
+            stops?.map((stop, index) => {
+              return (
+                <Marker
+                  key={index}
+                  position={stop.location}
+                  options={{
+                    icon: {
+                      url:
+                        (darkModeEnabled && selectedStop !== index) ||
+                        (!darkModeEnabled && selectedStop === index)
+                          ? blueStopMarker
+                          : purpleStopMarker,
+                      scaledSize: new google.maps.Size(35, 50),
+                      origin: new google.maps.Point(0, 0),
+                      anchor: new google.maps.Point(17.5, 50),
                     },
-                    0.05
-                  );
-                  map.fitBounds(bounds, padding);
-                  onMarkerSelect && onMarkerSelect(stop);
-                }}
-              />
-            );
-          })}
-      </GoogleMap>
-    </LoadScript>
+                  }}
+                  onClick={() => {
+                    setSelectedStop(index);
+                    const bounds = getBounds(
+                      {
+                        lat: stop.location.lat(),
+                        lng: stop.location.lng(),
+                      },
+                      0.05
+                    );
+                    map.fitBounds(bounds, padding);
+                    onMarkerSelect && onMarkerSelect(stop);
+                  }}
+                />
+              );
+            })}
+        </GoogleMap>
+      </LoadScript>
+    </>
   );
 };
