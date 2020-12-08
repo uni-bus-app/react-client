c763376ce6b0bfec0104d70bd3479ea9b1e04915
Jools Hattey <jools.hg@gmail.com>
2020-12-08 20:07:42 +0000

    added api keys to separate file

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
 export const config = {
   mapsApiKey: 'AIzaSyDkT81ky0Yn3JYuk6bFCsq4PVmjXawppFI',
+  apiUrl: 'https://20200817t190317-dot-unibus-app.nw.r.appspot.com',
+  // apiUrl: 'http://localhost:8080',
 };
