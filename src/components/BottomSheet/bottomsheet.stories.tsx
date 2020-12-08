f3383733989f905a685f28895848345738cf0449
Reece Birchmore <reecebirchmore@gmail.com>
2020-12-08 00:24:29 +0000

    BottomSheet Component

---
+++ src/components/BottomSheet/bottomsheet.stories.tsx
@@ -0,0 +1,15 @@
+import BottomSheet from './bottomsheet';
+import React, { useRef } from 'react';
+
+export default {
+  title: 'BottomSheet',
+  component: BottomSheet,
+};
+
+const Template = (args) => <BottomSheet {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  title: 'TEST',
+};
---
+++ src/components/BottomSheet/bottomsheet.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from 'react';
+import Sheet from 'react-modal-sheet';
+
+interface BottomSheetProps {
+  children: ReactNode;
+  title: string;
+}
+
+export default function BottomSheet(props: BottomSheetProps) {
+  const [isOpen, setOpen] = React.useState(false);
+
+  const { children, title } = props;
+
+  return (
+    <>
+      <button onClick={() => setOpen(true)}>Open sheet</button>
+
+      <Sheet isOpen={isOpen} onClose={() => setOpen(false)}>
+        <Sheet.Container>
+          <Sheet.Header>{title}</Sheet.Header>
+          <Sheet.Content>{children}</Sheet.Content>
+        </Sheet.Container>
+        <Sheet.Backdrop />
+      </Sheet>
+    </>
+  );
+}
