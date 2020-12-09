0a6a0317e3ab234bee60907aa33abaccfc136eeb
Reece Birchmore <reecebirchmore@gmail.com>
2020-12-09 00:41:07 +0000

    BottomSheet Component: Updated file

--- src/components/BottomSheet/bottomsheet.tsx
+++ src/components/BottomSheet/bottomsheet.tsx
@@ -4,16 +4,17 @@ import Sheet from 'react-modal-sheet';
 interface BottomSheetProps {
   children: ReactNode;
   title?: string;
+  open: boolean;
 }
 
 export default function BottomSheet(props: BottomSheetProps) {
-  const [isOpen, setOpen] = React.useState(false);
+  const { children, title, open } = props;
 
-  const { children, title } = props;
+  const [isOpen, setOpen] = React.useState(open);
 
   return (
     <>
-      <Sheet isOpen={props.open} onClose={() => setOpen(false)}>
+      <Sheet isOpen={isOpen} onClose={() => setOpen(false)}>
         <Sheet.Container>
           <Sheet.Header>{title}</Sheet.Header>
           <Sheet.Content>{children}</Sheet.Content>
