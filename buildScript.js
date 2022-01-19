476c5b062fdcd7954773e38bbd12fc655779620a
Jools Hattey <jools.hg@gmail.com>
2022-01-19 23:41:42 +0000

    moved secrets to environment variables

--- buildScript.js
+++ buildScript.js
@@ -1,11 +1,25 @@
 const replace = require('replace-in-file');
+require('dotenv').config();
 
-const value = process.env.ANALYTICS === 'enabled';
+const isProd = process.env.ANALYTICS === 'enabled';
+const mapsAPIKey = process.env.MAPS_API_KEY;
+const prodFirebaseConfig = process.env.PROD_FIREBASE_CONFIG;
+const devFirebaseConfig = process.env.DEV_FIREBASE_CONFIG;
 
 const options = {
   files: './src/config.ts',
-  from: /isProd = false/g,
-  to: 'isProd = ' + value.toString(),
+  from: [
+    /isProd = false/g,
+    /mapsApiKey = ''/g,
+    /prodFirebaseConfig = '{}'/g,
+    /devFirebaseConfig = '{}'/g,
+  ],
+  to: [
+    `isProd = ${isProd.toString()}`,
+    `mapsApiKey = '${mapsAPIKey}'`,
+    `prodFirebaseConfig = ${prodFirebaseConfig}`,
+    `devFirebaseConfig = ${devFirebaseConfig}`,
+  ],
   allowEmptyPaths: false,
 };
 
@@ -16,7 +30,7 @@ try {
       "Please make sure that file '" + options.files + "' has \"isProd = ''\""
     );
   }
-  console.log('Production analytics enabled: ', value.toString());
+  console.log('Production analytics enabled: ', isProd.toString());
 } catch (error) {
   console.error('Error occurred:', error);
   throw error;
