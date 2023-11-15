7c4610dccc7e2f8ec17bcdc216afaf7a401c6355
Reece <reecebirchmore@gmail.com>
2023-11-15 21:42:44 +0000

    Merge branch 'create-setup-screen' into newMainBranch

---
+++ src/beta-components/Carousel/carousel.tsx
@@ -0,0 +1,98 @@
+import { Button } from '@mui/material';
+import React, { cloneElement, useEffect, useState } from 'react';
+import { useSettings } from '../SettingsProvider';
+import { SettingsItemsNames } from '../SettingsProvider/types';
+import './styles.css';
+
+export const CarouselItem = ({ children, width }: any) => {
+  return <div className="carousel-item">{children}</div>;
+};
+
+export const Carousel = ({ children }: any) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const settings = useSettings();
+
+  const updateIndex = (newIndex: any) => {
+    if (newIndex < 0) {
+      newIndex = React.Children.count(children) - 1;
+    } else if (newIndex >= React.Children.count(children)) {
+      newIndex = 0;
+    }
+    setActiveIndex(newIndex);
+  };
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      () => {
+        settings.setValue(SettingsItemsNames.UseLocation, true);
+        // updateIndex(activeIndex + 1);
+      },
+      (e) => {
+        settings.setValue(SettingsItemsNames.UseLocation, false);
+        // updateIndex(activeIndex + 1);
+      }
+    );
+  };
+
+  const settingsObjectSetup = (allow: boolean) => {
+    if (activeIndex === 1) {
+      allow === true
+        ? getLocation()
+        : settings.setValue(SettingsItemsNames.UseLocation, false);
+      console.log('fuckin bean');
+    }
+    if (allow === false || activeIndex === 0) {
+      updateIndex(activeIndex + 1);
+    }
+  };
+
+  return (
+    <div className="Carousel">
+      <header>
+        <span
+          className="Carousel-header"
+          onClick={() => updateIndex(React.Children.count(children))}
+        >
+          {activeIndex !== React.Children.count(children) - 1 && 'SKIP SETUP'}
+        </span>
+      </header>
+      <div
+        className="inner"
+        style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+      >
+        {React.Children.map(children, (child, index) => {
+          return cloneElement(child, { width: '100%' });
+        })}
+      </div>
+
+      <div className="Carousel-footer">
+        <Button
+          className="button"
+          variant="contained"
+          size="large"
+          onClick={() => settingsObjectSetup(true)}
+        >
+          {activeIndex === 0 ||
+          activeIndex === React.Children.count(children) - 1
+            ? 'Continue'
+            : 'Enable'}
+        </Button>
+        {activeIndex !== 0 &&
+          activeIndex !== React.Children.count(children) - 1 && (
+            <Button
+              variant="text"
+              className="smallText"
+              sx={{ color: 'grey', fontStyle: 'none' }}
+              onClick={() => {
+                settingsObjectSetup(false);
+              }}
+            >
+              No Thanks
+            </Button>
+          )}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
